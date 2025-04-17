0d0a170f2b918cca9cfd68aafff805a26a021613
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2025-04-17 17:15:04 +0530

    Update Check for Equal Occurrences of ‘cat’ and ‘hat’ in a String.py

--- Check for Equal Occurrences of ‘cat’ and ‘hat’ in a String.py
+++ Check for Equal Occurrences of ‘cat’ and ‘hat’ in a String.py
@@ -1,3 +1,6 @@
+'''You are given a string str, you need to return True if  the words "cat" and "hat" appear same number of times in str, otherwise return False.
+Note: str contains only lowercase English alphabets.'''
+
 str=input("Enter any string containing cat and hat word: ")
 def CatHat(str):
     if(str.count("cat")==str.count("hat")):
