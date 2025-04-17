560347bb8a22c349e5dbd1327ccc2a99472a225c
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2025-04-17 17:14:15 +0530

    Add files via upload

---
+++ Check for Equal Occurrences of ‘cat’ and ‘hat’ in a String.py
@@ -0,0 +1,9 @@
+str=input("Enter any string containing cat and hat word: ")
+def CatHat(str):
+    if(str.count("cat")==str.count("hat")):
+        print("True")
+    else:
+        print("false")
+CatHat(str)
+    
+
