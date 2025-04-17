d2482c19329c539f8bfa5c680c43fd3ec70fe887
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2025-04-17 17:10:58 +0530

    Update Check the status.py

--- Check the status.py
+++ Check the status.py
@@ -1,3 +1,11 @@
+'''Given two integer variables a and b, and a boolean variable flag. The task is to check the status and return accordingly.
+
+Return True for the following cases:
+
+Either a or b (not both) is non-negative and the flag is false.
+Both a and b are negative and the flag is true.
+Otherwise, return False.'''
+           
 a=int(input("Enter the value of a: "))
 b=int(input("Enter the va;ue of b: "))
 flag=input("Enter the value of flag in terms of true and false: ")
